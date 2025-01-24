25be892869bc5c38f1b919eda0f26a8c4090ba5d
kieferperquin <kieferperquin@gmail.com>
2025-01-24 21:43:19 +0100

    some changes

--- Assets/Scripts/Bubble/BubbleConnectionManager.cs
+++ Assets/Scripts/Bubble/BubbleConnectionManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class BubbleConnectionManager : MonoBehaviour
 {
-    /*
-        i need to check for when i select if i am on a bubble
-        if i am on a bubble then i need to put it in a list 
-        and while holding down the select button and i hover over 
+    /// <summary> steps to take
+    /// 
+    /// 1. i need to check for when i select if i am on a bubble
+    /// 2. if i am on a bubble then i need to put it in a list 
+    /// 3. and while holding down the select button and i hover over a different bubble that is not in the list do step 2
+    /// 4. if i release the select button than the list will be reset
+    /// 5. if i hover over the "player" then then i will send the list of strings to somethings james will make to check if the centence is correct
+    /// 
+    /// </summary>
+     
+    List<GameObject> selectedBubbles = new List<GameObject>();
 
-
-
-    */
     void Start()
     {
         
@@ -21,4 +25,29 @@ public class BubbleConnectionManager : MonoBehaviour
     {
         
     }
+
+    public void Select()
+    {
+
+    }
+
+    void SendDataForConnection()
+    {
+        if (selectedBubbles.Count <= 1) return;
+
+        GameObject pointA = selectedBubbles[selectedBubbles.Count - 2];
+        GameObject pointB = selectedBubbles[selectedBubbles.Count - 1];
+
+        /// send pointA and pointB to connectionvisualizer script
+    }
 }
+    /*
+    void connectionvisualizer(Transform a, Transform b)
+    {
+
+    }
+
+    void connectionvisualizer(GameObject a, GameObject b)
+    {
+        connectionvisualizer(a.transform, b.transform);
+    }*/
