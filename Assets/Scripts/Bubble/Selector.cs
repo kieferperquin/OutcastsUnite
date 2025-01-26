c75a6ba2b98ab509464e2009df4cfb1eaebf82e3
JamesKessels <JamesKessels@users.noreply.github.com>
2025-01-26 13:29:24 +0100

    Buildtests

--- Assets/Scripts/Bubble/Selector.cs
+++ Assets/Scripts/Bubble/Selector.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
 
 public class Selector : MonoBehaviour
 {
